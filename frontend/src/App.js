e67a3c3d513db48b86de258792e17441a6d93871
sinamohsenifar <sinamohsenifar@gmail.com>
2021-12-13 01:15:18 +0330

    the navbar is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App ">
+      <Header />
+      <Container>
+        <main>
+          <h2>Welcom to store</h2>
+        </main>
+      </Container>
+      <Footer />
     </div>
   );
 }
