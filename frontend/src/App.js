2fd8b64923da48a95024e942b01e518b62fd322a
sinamohsenifar <sinamohsenifar@gmail.com>
2021-12-13 06:08:57 +0330

    bootstrap themes are collected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Container } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
+
 
 function App() {
   return (
@@ -10,6 +11,12 @@ function App() {
       <Container>
         <main>
           <h2>Welcom to store</h2>
+          <div>
+            <Button>button</Button>
+          </div>
+          <div>
+            <button className='btn btn-success'>button</button>
+          </div>
         </main>
       </Container>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './bootstrap.min.css'
+import './bootstrapThemes/quartz.min.css'
 
 
 ReactDOM.render(
